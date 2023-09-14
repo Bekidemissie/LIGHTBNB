ed72d9d7b40ec8570b9917d14e005560e0892943
Bekidemissie <bekidemissie16@gmail.com>
2023-09-14 15:23:23 -0400

    dry my code

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -6,16 +6,12 @@ const pool = new Pool({
   user: 'labber',
   password: 'labber',
   host: 'localhost',
-  port : 5432,
+  port: 5432,
   database: 'lightbnb'
 });
 pool.connect()
-{
-  console.log("connected my db")
-
- }
-//pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)});
-/// Users
+  .then(() => console.log("connected my db"))
+  .catch(err => console.error('An error occurred:', err));
 
 /**
  * Get a single user from the database given their email.
@@ -23,22 +19,14 @@ pool.connect()
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  // let resolvedUser = null;
-  // for (const userId in users) {
-  //   const user = users[userId];
-  //   if (user && user.email.toLowerC 'ase() === email.toLowerCase()) {
-  //     resolvedUser = user;
-  //   }
-  // }
-  // return Promise.resolve(resolvedUser);
-return pool.query('SELECT * FROM users WHERE email = $1' ,[email])
-.then(data => {
-  return data.rows[0]
-})
-.catch(err => {
-  console.error('An error occurred:', err);
-  throw err; 
-});
+  return pool.query('SELECT * FROM users WHERE email = $1', [email])
+    .then(data => {
+      return data.rows[0]
+    })
+    .catch(err => {
+      console.error('An error occurred:', err);
+      throw err;
+    });
 
 };
 
@@ -48,17 +36,15 @@ return pool.query('SELECT * FROM users WHERE email = $1' ,[email])
  * @return {Promise<{}>} A promise to the user.
  */
 // const getUserWithId = function (id) {
-//  // return Promise.resolve(users[id]);
 
-// };
-const getUserWithId = function(id) {
+const getUserWithId = function (id) {
   return pool.query('SELECT * FROM users WHERE id = $1', [id])
     .then(data => {
       return data.rows[0];
     })
     .catch(err => {
       console.error('An error occurred:', err);
-      throw err; 
+      throw err;
     });
 };
 
@@ -69,14 +55,7 @@ const getUserWithId = function(id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-// const addUser = function (user) {
-//   const userId = Object.keys(users).length + 1;
-//   user.id = userId;
-//   users[userId] = user;
-//   return Promise.resolve(user);
-// };
-
-const addUser = async function(user) {
+const addUser = async function (user) {
   const query = `
     INSERT INTO users (name, email, age)  -- specify the column names here
     VALUES ($1, $2, $3)                    -- use parameterized queries for security
@@ -93,20 +72,12 @@ const addUser = async function(user) {
     throw err;
   }
 };
-
-
-
-/// Reservations
-
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-// const getAllReservations = function (guest_id, limit = 10) {
-//   return getAllProperties(null, 2);
-// };
-const getAllReservations = async function(guest_id, limit = 10) {
+const getAllReservations = async function (guest_id, limit = 10) {
   const queryString = `
     SELECT *
     FROM reservations
@@ -127,24 +98,14 @@ const getAllReservations = async function(guest_id, limit = 10) {
 };
 
 
-
-
-/// Properties
-
 /**
  * Get all properties.
  * @param {{}} options An object containing query options.
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-// const getAllProperties = function (options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// };
-const getAllProperties = function(options, limit = 10) {
+
+const getAllProperties = function (options, limit = 10) {
   let queryParams = [];
   let queryString = `
     SELECT properties.*, avg(property_reviews.rating) as average_rating
@@ -178,22 +139,13 @@ const getAllProperties = function(options, limit = 10) {
 };
 
 
-
-
-
-
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-// const addProperty = function (property) {
-//   const propertyId = Object.keys(properties).length + 1;
-//   property.id = propertyId;
-//   properties[propertyId] = property;
-//   return Promise.resolve(property);
-// };
-const addProperty = async function(property) {
+
+const addProperty = async function (property) {
   const queryString = `
     INSERT INTO properties (
       owner_id,
@@ -242,11 +194,6 @@ const addProperty = async function(property) {
       throw err;
     });
 };
-
-
-
-
-
 
 
 module.exports = {
